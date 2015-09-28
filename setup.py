63d7ac8f51a23b19f0d6159ad7ac298179677881
Friedrich Lindenberg <friedrich@pudo.org>
2015-09-28 16:54:22 +0200

    up the version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jsonmapping',
-    version='0.3.5',
+    version='0.4.0',
     description="Map flat data to structured JSON via a mapping.",
     long_description="",
     classifiers=[
